fb4edd4b0c4215218a69ff92589e9b1b59a9e8e8
Maciej Sypniewski <msypniewski511@gmail.com>
2017-01-11 20:28:00 +0100

    Validacja modelu Product

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,53 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "product attributes must be not empty!" do
+  	product = Product.new
+  	assert_not product.valid?
+  	assert product.errors[:title].any?
+  	assert product.errors[:description].any?
+  	assert product.errors[:image_url].any?
+  	assert product.errors[:price].any?
+  end
+
+  test "product price must be positive" do
+  	product = Product.new(title: 'tytul', description: 'Opis', image_url: 'plik.jpg')
+  	assert !product.valid?
+  	product.price = -1
+  	assert product.invalid?
+  	assert_equal "must be greater than or equal to 0.01", product.errors[:price].join('; ')
+
+  	product.price = 0
+  	assert product.invalid?
+  	assert_equal "must be greater than or equal to 0.01", product.errors[:price].join('; ')
+
+  	product.price = 1
+  	assert product.valid?
+  end
+
+  def new_product(image_url)
+  	Product.new(title: 'tytul', description: 'opis', price: 1, :image_url => image_url)
+  end
+
+  test "product image should have valid extensions" do
+  	ok = %w{maciej.gif xxx.gif xxx.png XXX.JPG XXX.PNG XXX.GIF}
+  	bad = %w{xxx.doc xxx.gif.more}
+  	ok.each do |url| 
+  	  assert new_product(url).valid?
+  	end
+
+  	bad.each do |url|
+  		assert !new_product(url).valid?
+  	end
+  	
+  end
+
+  test "the title of product should be unique" do
+  	product = Product.new(title: products(:ruby).title,
+  		description: 'opis',
+  		image_url: 'xxx.jpg',
+  		price: 10.0)
+  	assert !product.save
+  end
 end
