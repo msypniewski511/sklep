179ee0f44d7c94b5ab11b247141d1e6cdeae5c37
Maciej Sypniewski <msypniewski511@gmail.com>
2017-01-23 18:21:58 +0100

    Finished cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def current_cart
+  	begin
+  		Cart.find(session[:cart_id])
+  	rescue ActiveRecord::RecordNotFound => e
+  		cart = Cart.create
+  		session[:cart_id] = cart.id
+  		cart
+  	end
+  end
+
+  def counter
+      if session[:counter].nil?
+        session[:counter] = 1
+      else
+        session[:counter] = session[:counter] + 1
+      end
+      return session[:counter]
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+	has_many :line_items
+
 	validates :title, :description, :image_url, :presence => true
 	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 	validates :title, :uniqueness => true
@@ -6,4 +8,16 @@ class Product < ApplicationRecord
 		:with => /\.(gif|jpg|png)\z/i,
 		:message => 'must be URL for GIF, JPG or PNG file'
 	}
+
+	before_destroy :ensure_not_referenced_by_any_line_item
+
+	private
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'Line Item present')
+			return false
+		end
+	end
 end
